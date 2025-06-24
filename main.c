d05b584d703cc7659aee97e3c7e987a7c639082e
Virellus <switzerwallace@gmail.com>
2025-06-24 12:52:34 -0500

    claned up a lot and error handling

--- main.c
+++ main.c
@@ -13,14 +13,14 @@ Student NetID: wjs291
 int main(int argc, char *argv[]){   // argc is the count of cli arguments, argv arrary of strings containing the arguments
     //verify
     if (argc != 5){
-        printf("Incorrect number of arguments!");
+        printf("Error incorrect number of arguments!"); //checking if the user gave the worng num of inputs
         return 1;
     }
     // declaring variables
     char inBuf[BUF_SIZE];           // stores the raw data file in a box 
     char outBuf[BUF_SIZE];          // stores prepared output in a box
     char line[BUF_SIZE];            // puts the indv lines from the inBuff so they can be processed
-    int readLen;                    // used later but will sotre how mnay bytes were read
+    int readLen;                    // used later but will store how mnay bytes were read
     int offset = 0;                 // used to recall where in file the program is 
     
     //intitalize the arguments
@@ -30,12 +30,27 @@ int main(int argc, char *argv[]){   // argc is the count of cli arguments, argv 
     int buzzNum = atoi(argv[4]);
     // attempts to open file
     int fd_inFile = open(inFile, O_RDONLY);
+    if (fd_inFile == -1) {                      // making sure opens 
+        perror("Error opening input file");
+        return 1;
+    }
     int fd_outFile = open(outFile, O_WRONLY|O_CREAT|O_TRUNC, 0644);
+    if (fd_outFile == -1) {                     // making sure opens 
+        perror("Error opening output file");
+        return 1;
+    }
     //Process 
     while ((readLen = pread(fd_inFile, inBuf, BUF_SIZE, offset))> 0){   //loops through the file until over
         // starts at 0 and reads until line is over or buffer is full
         //returns number of bytes actually read
         // find new line char in buffer
+
+        if (readLen > BUF_SIZE){
+            printf("Error read lengthn larger than buffer size\n");
+            close(fd_inFile);
+            close(fd_outFile);
+            return 1;
+        }
         int lineLen = 0;
         for (int i = 0; i < readLen; i ++){ // loops through charcters in the buffer to find end of the line
             if (inBuf[i] == '\n') {
@@ -53,19 +68,33 @@ int main(int argc, char *argv[]){   // argc is the count of cli arguments, argv 
         // convert strings to int
         int number = atoi(line);
         // creating a output starting with the number
-        int outLen = snprintf(outBuf, BUF_SIZE, "%d", number);
+        int outLen = snprintf(outBuf, BUF_SIZE, "%d ", number);
         // fizz check
         if (number % fizzNum == 0){
-            outLen += snprintf(outBuf + outLen, BUF_SIZE - outLen, " Fizz");
+            outLen += snprintf(outBuf + outLen, BUF_SIZE - outLen, "Fizz");
         }
         if (number % buzzNum == 0){
-            outLen += snprintf(outBuf + outLen, BUF_SIZE - outLen, " Buzz");
+            outLen += snprintf(outBuf + outLen, BUF_SIZE - outLen, "Buzz");
         }
         // adding new line to end of output
         outLen += snprintf(outBuf + outLen, BUF_SIZE - outLen, "\n");
         // writing results to outfile
-        write(fd_outFile, outBuf, outLen);
+        //write(fd_outFile, outBuf, outLen);
+
+        if (write(fd_outFile, outBuf, outLen) == -1){
+            perror("Error writing to output file");
+            close(fd_inFile);
+            close(fd_outFile);
+            return 1;
+        }
+        
         offset += lineLen + 1;
+    }
+    if (readLen == -1){
+        perror("Error reading form the input file");
+        close(fd_inFile);
+        close(fd_outFile);
+        return 1;
     }
     
     close(fd_inFile);
